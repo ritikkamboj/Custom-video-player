5b7afe1b4b6920fbad5a130ceba8c81641a9d7e8
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-08-08 12:57:46 +0530

    applied js on progress bar and timestamp

--- script.js
+++ script.js
@@ -11,33 +11,49 @@ function toggleVideoStatus() {
   else video.pause();
 }
 function updatePlayIcon() {
-//   console.log("pause waala ");
-  if(video.paused)
-  {
-    play.innerHTML = ' <i class="fa fa-play fa-2x"></i>'
-  }
-  else
-  play.innerHTML =' <i class="fa fa-pause fa-2x"></i>'
-
+  //   console.log("pause waala ");
+  if (video.paused) {
+    play.innerHTML = ' <i class="fa fa-play fa-2x"></i>';
+  } else play.innerHTML = ' <i class="fa fa-pause fa-2x"></i>';
 }
 // function updatePlayIcon2()
 // {
 //     console.log('play waala')
 // }
 
-function updateProgress() {}
+function updateProgress() {
+//   console.log(video.currentTime);
+console.log(progress.value)
+progress.value = parseInt((video.currentTime/video.duration)*100);
+
+// here we have to found mins and secs 
+let mins ;
+mins = Math.floor(video.currentTime/60);
+if(mins < 10)
+{
+    mins = '0'+mins;
+}
+let secs ;
+secs = Math.floor(video.currentTime%60);
+if(secs < 10)
+{
+    secs = '0'+secs;
+}
+timestamp.innerHTML = `${mins}:${secs}`
+}
 
 function stopVideo() {
-    video.timestamp=0;
-    video.pause();
-
+  video.timestamp = 0;
+  video.pause();
 }
-function setVideoProgress() {}
+function setVideoProgress() {
+    video.currentTime = (+progress.value * video.duration)/100;
+}
 
 video.addEventListener("click", toggleVideoStatus);
 video.addEventListener("pause", updatePlayIcon);
 video.addEventListener("play", updatePlayIcon);
-video.addEventListener("timestamp", updateProgress);
+video.addEventListener("timeupdate", updateProgress);
 
 play.addEventListener("click", toggleVideoStatus);
 // stop.addEventListener('click',stopVideo);
