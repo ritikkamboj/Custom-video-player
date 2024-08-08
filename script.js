9eb07bcef0a0521b7e8b5e89bddf765ba08cc1a1
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-08-08 12:17:32 +0530

    pause play stop functionality on icon applied

--- script.js
+++ script.js
@@ -3,20 +3,35 @@ const play = document.getElementById("play");
 const stop = document.getElementById("stop");
 const progress = document.getElementById("progress");
 const timestamp = document.getElementById("timestamp");
-console.log(video.pause)
+// console.log(video.pause)
 
 function toggleVideoStatus() {
-    console.log(video.paused);
-    if(video.paused)
-        video.play()
-    else
-    video.pause(); 
+  console.log(video.paused);
+  if (video.paused) video.play();
+  else video.pause();
 }
-function updatePlayIcon() {}
+function updatePlayIcon() {
+//   console.log("pause waala ");
+  if(video.paused)
+  {
+    play.innerHTML = ' <i class="fa fa-play fa-2x"></i>'
+  }
+  else
+  play.innerHTML =' <i class="fa fa-pause fa-2x"></i>'
+
+}
+// function updatePlayIcon2()
+// {
+//     console.log('play waala')
+// }
 
 function updateProgress() {}
 
-function stopVideo() {}
+function stopVideo() {
+    video.timestamp=0;
+    video.pause();
+
+}
 function setVideoProgress() {}
 
 video.addEventListener("click", toggleVideoStatus);
@@ -29,5 +44,3 @@ play.addEventListener("click", toggleVideoStatus);
 stop.addEventListener("click", stopVideo);
 
 progress.addEventListener("change", setVideoProgress);
-
-
